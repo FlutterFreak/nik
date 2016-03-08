fce9a84968966a869916f345c555546e38065090
a <a@a.com>
2016-03-08 10:01:35 +0000

    u

---
+++ php/activity2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1516734
+ * Date: 08/03/2016
+ * Time: 09:43
+ */
+$myage = 28;
+
+if ($myage>=21){
+    echo "you can buy specs,mugs,sausage rolls"
+}
+elseif($myage>=18){
+    echo "you can buy specs and mugs"
+}
+elseif($myage>=16){
+    echo "you can buy specs"
+}
+else{
+    echo "you can't buy anything"
+}
+?>
