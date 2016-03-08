e86e94ad973b3f8d7d0a51320ac0574110e3a781
a <a@a.com>
2016-03-08 10:25:00 +0000

    u

--- php/activity3.php
+++ php/activity3.php
@@ -5,3 +5,20 @@
  * Date: 08/03/2016
  * Time: 10:08
  */
+$wantedgood = "mugs";
+
+switch ($wantedgood) {
+    case "specs":
+        echo "You have to be 16 to buy specs";
+        break;
+    case "mugs":
+        echo "You have to be 18 to buy mugs";
+        break;
+    case "sausage rolls":
+        echo "You have to be 21 to buy sausage rolls";
+        break;
+    default:
+        echo "You haven't selected a thing to buy";
+}
+
+?>
