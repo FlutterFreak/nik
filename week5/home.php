bcdfc2c548342bec09b5f4bdda5d96718033bb6d
a <a@a.com>
2016-03-14 10:58:10 +0000

    u

---
+++ week5/home.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<h1>Hello</h1>
+</body>
+</html><?
+/**
+ * Created by PhpStorm.
+ * User: 1516734
+ * Date: 14/03/2016
+ * Time: 10:57
+ */
