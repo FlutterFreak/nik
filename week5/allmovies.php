07e5912eed4c1f7b53a3dd503e48e2be176f6363
a <a@a.com>
2016-03-15 10:51:17 +0000

    u

---
+++ week5/allmovies.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>All Movies</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>All Movies</body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1516734
+ * Date: 15/03/2016
+ * Time: 10:21
+ */
+include("databse.php");
+$db=marvelmovies;
+$sq1=SELECT*FROM  marvelmovies;
+$result=mysqli_query($db,$sql);
+while($row=$result->fetch_array())
+{
+    $movietitle=$row['title'];
+    echo"<p>".$movietitle."<p>";
+}
---
+++ week5/xmenfilms.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1516734
+ * Date: 15/03/2016
+ * Time: 10:22
+ */
