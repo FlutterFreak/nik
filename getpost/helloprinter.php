87d6150c42c87ea2aa607985588c9fe4884f2b12
a <a@a.com>
2016-03-21 10:47:46 +0000

    u

---
+++ getpost/helloprinter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1516734
+ * Date: 21/03/2016
+ * Time: 10:27
+ */
---
+++ getpost/helloselector.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Where would you like to say hello</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>Where would you like to say hello</body>
+<ul>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1516734
+ * Date: 21/03/2016
+ * Time: 10:26
+ */
+$planet1 = $_GET["Earth"];
+$planet2 = $_GET["mars"];
+$planet3 = $_GET["uranus"];
+ echo $planet1;
+ echo $planet2;
+ echo $planet3;
+
+?>
+    </ul>
+</html>
