a89626e8280dc172799585fb83bfc8c4779206d4
a <a@a.com>
2016-03-21 09:59:06 +0000

    u

--- week5/databse.php
+++ week5/databse.php
@@ -5,7 +5,7 @@
  * Date: 14/03/2016
  * Time: 10:19
  */
-define('DB_server','ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_SERVER','ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME','b126171871fa5a');
 define('DB_PASSWORD','a0abb12e');
 DEFINE('DB_DATABASE','nikdata');
