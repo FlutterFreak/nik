6d5d7f4ce673750e9ff78be6e9d6c08fd28821d4
a <a@a.com>
2016-03-21 09:05:50 +0000

    u

--- week5/after2010.php
+++ week5/after2010.php
@@ -1,3 +1,10 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Movies after 2010</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>Movies after 2010</body>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +12,13 @@
  * Date: 15/03/2016
  * Time: 10:22
  */
+include("databse.php");
+$db=marvelmovies;
+$sq1= SELECT * FROM marvelmovies;
+     Where yearReleased >= 2010;
+$result=mysqli_query($db,$sql);
+while($row=$result->fetch_array())
+{
+    $movietitle=$row['title'];
+    echo"<p>".$movietitle."<p>";
+}
