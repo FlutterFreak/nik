79fc8f9ef0f73710ccf3a9cc6f14b69665f59add
a <a@a.com>
2016-03-22 09:55:49 +0000

    u

--- superheros/index.php
+++ superheros/index.php
@@ -11,16 +11,26 @@
 <h2>Superhero Home page</h2>
 </header>
 <p>What would you like to choose</p>
-<>
+<ul>
     <li><a href="insertSuperhero.php">Insert a superhero</a></li>
     <li><a href="displaySuperhero.php">Display all superheros</a></li>
     <li><a href="battle.php">Insert a battle</a></li>
     <li><a href="displayBattles.php">Display all battles</a></li>
     <ul>
         <?
-
+        include ("db_connect.php");
+        $sql_query = "SELECT * FROM superheros";
+        $result = $db->query($sql_query);
+        while($row = $result->fetch_array())
+        {
+            $fristname = $row['firstname'];
+            $lastname = $row['lastname'];
+            $id = $row['superheroID'];
+            echo "<li><a href='displaybattles.php?id={$id}'> Battles for {$firstname} {$lastname} </a></li>";
+        }
         ?>
+        </ul>
 
 </ul>
 </body>
-</html><?
+</html>
