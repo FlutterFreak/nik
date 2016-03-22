6b0a3e1c9638446e33c4a57c2c95562d1f572154
a <a@a.com>
2016-03-22 10:57:50 +0000

    u

--- superheros/battle.php
+++ superheros/battle.php
@@ -13,4 +13,23 @@
 </header>
 <main>
     <form action="insertbattle.php" method="post">
-        <p>Select the superhero that fought in this battle</p><select name="superhero"
+        <p>Select the superhero that fought in this battle</p><select name="superhero">
+            <?
+            include ("db_connect.php");
+            $sql_query = "SELECT * FROM superheros";
+            $result = $db->query($sql_query);
+            while($row = $result->fetch_array())
+            {
+            $fristname = $row['firstname'];
+            $lastname = $row['lastname'];
+            $superheroID = $row['superheroID'];
+            echo "<option value='{superheroID}'>{$firstname} {lastname}</option>";
+            ?>
+        </select><br>
+        <input type="text" name="villian" placeholder="Villian Faught"><br>
+        <input type="submit" text="Record Battle">
+        </form>
+    </main>
+</body>
+</main>
+</html>
