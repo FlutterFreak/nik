6543fdd2790c8caf37f3cb5c78ba3d80a5c60900
a <a@a.com>
2016-03-22 10:19:41 +0000

    u

---
+++ superheros/insertsuperhero.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1516734
+ * Date: 22/03/2016
+ * Time: 10:08
+ */
+include ("db_connect.php");
+
+$firstname = $_post["firstname"];
+$lastname = $_post["lastname"];
+$superpower = $_post["superpower"];
+$sql = "INSERT INTO superheros (firstname, lastname, superpower) VALUES ('$firstname','$lastname','$superpower')";
+
+if (mysqli_query($db,$sql)){}
+else {
+     echo "Error:" . $sql . "<br>" . msqli_error(£db);
+    }
+header("location:index.php");
+?>
