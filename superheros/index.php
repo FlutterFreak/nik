44cb6916a88ebab46f8b3d971c000034100f33e5
a <a@a.com>
2016-03-22 09:42:31 +0000

    u

---
+++ superheros/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Superhero System</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<header>
+<h1>The Superhero System </h1>
+<h2>Superhero Home page</h2>
+</header>
+<p>What would you like to choose</p>
+<>
+    <li><a href="insertSuperhero.php">Insert a superhero</a></li>
+    <li><a href="displaySuperhero.php">Display all superheros</a></li>
+    <li><a href="battle.php">Insert a battle</a></li>
+    <li><a href="displayBattles.php">Display all battles</a></li>
+    <ul>
+        <?
+
+        ?>
+
+</ul>
+</body>
+</html><?
